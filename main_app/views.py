f2088d914a07094aa580800bb129f5a672f068e8
kylerwan <kyler.y.wang@gmail.com>
2023-02-11 22:06:34 -0800

    implement details page

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Media
 
 # media =[
@@ -21,3 +22,26 @@ def media_index(request):
     return render(request,'media/index.html',{
         'media': media
     })
+
+def media_detail(request, media_id):
+    media = Media.objects.get(id=media_id)
+
+    return render(request, 'media/detail.html',{
+        'media': media
+    })
+
+class MediaCreate(CreateView):
+    model = Media
+    fields = '__all__'
+    def form_valid(self, form):
+        form.instance.user=self.request.user
+        return super().form_valid(form)
+
+
+class MediaUpdate(UpdateView):
+    model = Media
+    fields = '__all__'
+
+class MediaDelete(DeleteView):
+    model = Media
+    succes_url = '/cats'
